448a2f431c33f23d5ed54dd677fc69c8e9007c4b
Yonggang Luo <luoyonggang@gmail.com>
2016-01-21 17:03:24 +0800

    No python buffer for all.

--- build/finish.py
+++ build/finish.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, unicode_literals
+import sys
+from BoltUtils import Unbuffered
 
-from install import checkoutAll
+sys.stdout = Unbuffered(sys.stdout)
 
 if __name__ == '__main__':
   print('Running the finish script')
