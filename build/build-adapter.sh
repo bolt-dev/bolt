0076d3421071f7dc54efb8bf6a70fbf0f0c06978
Yonggang Luo <luoyonggang@gmail.com>
2016-01-22 00:27:34 +0800

    Now we could be able to pass args to mocha.

--- build/build-adapter.sh
+++ build/build-adapter.sh
@@ -5,6 +5,7 @@ cd ~/../comm/
 
 echo Mach with HOME: ${HOME} PWD:$PWD MOZCONFIG: ${MOZCONFIG}
 echo Building in TARGET_DIR: ${TARGET_DIR} BUILD_TRIPLE:$BUILD_TRIPLE
+export PATH=/c/Program\ Files/Git/bin:$PATH
 echo ${MOCHA_SCRIPT}
 eval ${MOCHA_SCRIPT}
 exit
